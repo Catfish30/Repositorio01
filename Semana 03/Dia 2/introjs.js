c9e0f6a3ddbbc0b1d7ec74bf0947429bba8a5525
Catfish30 <michael.huamani@tecsup.edu.pe>
2021-06-19 00:33:53 -0500

    palta viendome

---
+++ Semana 03/Dia 2/introjs.js
@@ -0,0 +1,40 @@
+console.log("Hello World");
+
+console.warn("Advertencia");
+
+console.error("Error");
+
+console
+
+class mesa{
+
+    imprimirNombre(){
+        console.log("Soy una mesa")
+    }
+
+}
+
+var tabla = new mesa();
+
+tabla.imprimirNombre();
+
+var saludo = "Hola, como estas?" // No usar var en html
+
+let otrosaludo ="Usar let en ves de var para variables"
+
+otrosaludo="Hola"
+
+console.log(otrosaludo);
+
+otrosaludo="Hola 2"
+
+console.log(otrosaludo);
+
+const name = "Michael"
+
+console.log(name);
+
+name = "Pina"
+
+console.log(name);
+
