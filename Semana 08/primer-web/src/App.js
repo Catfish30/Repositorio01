ff9f7db52ba592e8f7247c319b49a228057ad8bb
Catfish30 <michael.huamani@tecsup.edu.pe>
2021-07-28 21:56:00 -0500

    update

--- Semana 08/primer-web/src/App.js
+++ Semana 08/primer-web/src/App.js
@@ -1,27 +1,13 @@
 
-import { useState } from "react"
-import logo from './logo.svg';
-import './App.css';
-import Header from './components/header'
+export default function App(){
+let titulo = "Pinafiel"
 
-function App() {
-
-  const [numero,setNumero] = useState(0)
-  const sumarUnidad = () => setNumero(numero + 1)
-  const restarUnidad = () => setNumero(numero -1)
-  
-
-  return (
+  return(
     <div>
-      <Header nombre="Pinita"/>
-      <h1>Hola Pina</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis, totam.</p>  
-      <h1>{numero}</h1>
-      <button onClick={sumarUnidad}>Sumar</button>
-      <button onClick={restarUnidad}>Restar</button>
-      <img src={logo} alt="" />
-    </div>    
-  );
-}
+      <h1>{titulo}</h1>
+      <br />
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, ipsum?</p>
 
-export default App;
+    </div>
+  )
+}
